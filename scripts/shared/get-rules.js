c066fd6ae2f9eab0c7d39319a58be0d102007423
fisker <lionkay@gmail.com>
2019-06-22 18:20:25 +0800

    chore: fix build script

--- scripts/shared/get-rules.js
+++ scripts/shared/get-rules.js
@@ -10,7 +10,9 @@ function getRules(config) {
     useEslintrc: false,
   });
 
-  const {rules} = engine.config.baseConfig;
+  engine.executeOnText('');
+
+  const {rules} = engine.getConfigForFile('');
   const defs = engine.getRules();
 
   for (const ruleId of Object.keys(rules)) {
